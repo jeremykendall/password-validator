16badeb44bd1ea9655f1b3cda286ff12d5ba364b
Jeremy Kendall <jeremy@jeremykendall.net>
2014-01-04 22:28:51 -0600

    Updates copyright year

--- src/JeremyKendall/Password/Decorator/AbstractDecorator.php
+++ src/JeremyKendall/Password/Decorator/AbstractDecorator.php
@@ -4,7 +4,7 @@
  * Password Validator
  *
  * @link      http://github.com/jeremykendall/password-validator Canonical source repo
- * @copyright Copyright (c) 2013 Jeremy Kendall (http://about.me/jeremykendall)
+ * @copyright Copyright (c) 2014 Jeremy Kendall (http://about.me/jeremykendall)
  * @license   http://github.com/jeremykendall/password-validator/blob/master/LICENSE MIT
  */
 
